a17656a0193ab4a53de81f07a12e3d04d1d60fdd
Hyemin <star970909@gmail.com>
2025-02-10 16:16:46 +0900

    20250210 16:14 내 냉장고 페이지 최종수정
    - 들여쓰기, 줄바꿈, 변수명 수정
    - 작업 요청 / 작업 성공에 대한 로그 추가
    - 일일 수정내역 https://www.notion.so/2025-02-10-196ca1bab448803fbc18ddeb9e171f16?pvs=4

--- src/main/java/com/exam/controller/RefrigeratorController.java
+++ src/main/java/com/exam/controller/RefrigeratorController.java
@@ -1,8 +1,6 @@
 package com.exam.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,105 +23,103 @@ import jakarta.validation.constraints.Size;
 @Controller
 @Validated
 public class RefrigeratorController {
-    
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-    RefrigeratorService refrigeratorService;
-    GoodsService goodsService;
 
-    public RefrigeratorController(RefrigeratorService refrigeratorService, GoodsService goodsService) {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	RefrigeratorService refrigeratorService;
+	GoodsService goodsService;
+
+	public RefrigeratorController(RefrigeratorService refrigeratorService, GoodsService goodsService) {
 		this.refrigeratorService = refrigeratorService;
 		this.goodsService = goodsService;
 	}
-    
-    // 전체 냉장고 목록 보기
-    @GetMapping("/refrigerator")
-   public String main(Model m) {
-       
-      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-      MemberDTO memberDTO = (MemberDTO)auth.getPrincipal();
-      String userid = memberDTO.getUserid();
-      
-      List<RefrigeratorDTO> refrigeratorList = refrigeratorService.refrigeratorList(userid);
-      m.addAttribute("refrigeratorList", refrigeratorList);
-       
-      List<GoodsDTO> refGoodsList = goodsService.getAllStock();
-      m.addAttribute("refGoodsList", refGoodsList);
-       
-      return "refrigerator";
-   }
 
-    // 식재료 추가
-    @GetMapping("/refrigeratorAdd")
-    public String refrigeratorAdd(@RequestParam String gCode,
-    							  @RequestParam String gCategory,
-    							  @RequestParam String gName,
-    						  	
-    				              @Size(min = 1, max = 2)
-    							  @RequestParam String rStock,
-    							
-    							  Model m
-    							  ) {
-    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    	MemberDTO memberDTO = (MemberDTO)auth.getPrincipal();
-    	
-    	String userid = memberDTO.getUserid();
-    	
-    	RefrigeratorDTO refrigeratorDTO = new RefrigeratorDTO();
-    	refrigeratorDTO.setUserid(userid);
-    	refrigeratorDTO.setgCode(gCode);
-    	refrigeratorDTO.setgCategory(gCategory);
-    	refrigeratorDTO.setgName(gName);
-    	refrigeratorDTO.setrStock(Integer.parseInt(rStock));
-    	
-    	int n = refrigeratorService.refrigeratorAdd(refrigeratorDTO);
-    	
-    	return "refrigerator/refrigeratorAddSuccess";
-    }
-    
-    // 식재료 삭제
-    @GetMapping("refrigeratorDelete")
-    public String refrigeratorDelete(@RequestParam Integer num) {
-       
-       int n = refrigeratorService.refrigeratorDelete(num);
-       
-       return "redirect:refrigerator";
-    }
-    
-    // 식재료 보유 현황 조회
-    @GetMapping("getRefrigeratorStock")
-    public String getRefrigeratorStock(@RequestParam String gCode, Model m) {
-       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-       MemberDTO memberDTO = (MemberDTO)auth.getPrincipal();
-       
-       String userid = memberDTO.getUserid();
+	// 전체 냉장고 목록 보기
+	@GetMapping("/refrigerator")
+	public String main(Model m) {
 
-        int stock = refrigeratorService.getRefrigeratorStock(userid, gCode);
-        m.addAttribute("stock", stock);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		MemberDTO memberDTO = (MemberDTO) auth.getPrincipal();
+		String userid = memberDTO.getUserid();
 
-        return "/refrigerator";
-    }
-    
-    // 식재료 수량 변경
-    @GetMapping("updateRefrigeratorStock")
-    public String updateRefrigeratorStock(@RequestParam int num,
-                                 @RequestParam int amount) {
-       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-       MemberDTO memberDTO = (MemberDTO)auth.getPrincipal();
-       
-       String userid = memberDTO.getUserid();
-       
-       RefrigeratorDTO refrigeratorDTO = new RefrigeratorDTO();
-       refrigeratorDTO.setUserid(userid);
-       refrigeratorDTO.setrStock(amount);
-       refrigeratorDTO.setNum(num);
-       
-       
-       int n = refrigeratorService.updateRefrigeratorStock(refrigeratorDTO);
-       
-       return "refrigerator/refrigeratorUpdateSuccess";
-       
-    }
+		List<RefrigeratorDTO> refrigeratorList = refrigeratorService.refrigeratorList(userid);
+		m.addAttribute("refrigeratorList", refrigeratorList);
 
+		List<GoodsDTO> refGoodsList = goodsService.getAllStock();
+		m.addAttribute("refGoodsList", refGoodsList);
+
+		return "refrigerator";
+	}
+
+	// 식재료 추가
+	@GetMapping("/refrigeratorAdd")
+	public String refrigeratorAdd(@RequestParam String gCode,
+								  @RequestParam String gCategory,
+								  @RequestParam String gName,
+								  @Size(min = 1, max = 2) @RequestParam String rStock,
+								  Model m) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		MemberDTO memberDTO = (MemberDTO) auth.getPrincipal();
+
+		String userid = memberDTO.getUserid();
+		logger.info("CustomLOG[REQUEST]: 냉장고에서 새로운 상품 추가를 요청받음: userid:{}, 상품명:{}, 수량:{}", userid, gName, rStock);
+
+		RefrigeratorDTO refrigeratorDTO = new RefrigeratorDTO();
+		refrigeratorDTO.setUserid(userid);
+		refrigeratorDTO.setgCode(gCode);
+		refrigeratorDTO.setgCategory(gCategory);
+		refrigeratorDTO.setgName(gName);
+		refrigeratorDTO.setrStock(Integer.parseInt(rStock));
+
+		int n = refrigeratorService.refrigeratorAdd(refrigeratorDTO);
+		logger.info("CustomLOG[SUCCESS]: 냉장고에 새로운 상품이 정상적으로 추가됨: userid:{}, 상품명:{}, 수량:{}", userid, gName, rStock);
+
+		return "refrigerator/refrigeratorAddSuccess";
+	}
+
+	// 냉장고 상품 삭제
+	@GetMapping("refrigeratorDelete")
+	public String refrigeratorDelete(@RequestParam Integer num) {
+
+		logger.info("CustomLOG[REQUEST]: 냉장고에서 상품 삭제를 요청받음");
+		int n = refrigeratorService.refrigeratorDelete(num);
+		logger.info("CustomLOG[SUCCESS]: 냉장고에서 상품이 정상적으로 삭제됨");
+
+		return "redirect:refrigerator";
+	}
+
+	// 식재료 보유 현황 조회
+	@GetMapping("getRefrigeratorStock")
+	public String getRefrigeratorStock(@RequestParam String gCode, Model m) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		MemberDTO memberDTO = (MemberDTO) auth.getPrincipal();
+
+		String userid = memberDTO.getUserid();
+
+		int stock = refrigeratorService.getRefrigeratorStock(userid, gCode);
+		m.addAttribute("stock", stock);
+
+		return "/refrigerator";
+	}
+
+	// 냉장고 상품 수량 변경
+	@GetMapping("updateRefrigeratorStock")
+	public String updateRefrigeratorStock(@RequestParam int num, @RequestParam int amount) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		MemberDTO memberDTO = (MemberDTO) auth.getPrincipal();
+
+		String userid = memberDTO.getUserid();
+		logger.info("CustomLOG[REQUEST]: 냉장고 상품 수량 변경을 요청받음 userid:{}, 상품등록번호:{}, 변경할 수량:{}", userid, num, amount);
+
+		RefrigeratorDTO refrigeratorDTO = new RefrigeratorDTO();
+		refrigeratorDTO.setUserid(userid);
+		refrigeratorDTO.setrStock(amount);
+		refrigeratorDTO.setNum(num);
+
+		int n = refrigeratorService.updateRefrigeratorStock(refrigeratorDTO);
+		logger.info("CustomLOG[SUCCESS]: 냉장고에서 상품 수량이 정상적으로 변경됨 userid:{}, 상품등록번호:{}, 변경후 수량:{}", userid, num, amount);
+		
+		return "refrigerator/refrigeratorUpdateSuccess";
+	}
 
 }
--- src/main/resources/META-INF/resources/WEB-INF/views/refrigerator/refrigeratorUpdateSuccess.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/views/refrigerator/refrigeratorUpdateSuccess.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <script>
-  alert("수량 변경 성공");
+  alert("상품의 수량이 변경되었습니다.");
   location.href="/shop/refrigerator";
 </script>
